217f94684ca68ecd51a97eb60183d27a57d646d7
Marco <marcowky@qq.com>
2024-04-08 20:37:37 +0800

    feat: 添加了easyaivtuber的适配

---
+++ src/view.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+from utils.config import config
+
+os.chdir("src\\interface\\view\\EasyAIVtuber")
+
+info = config.get_use_interface("view")
+
+def run_command():
+    command = [
+        "python", "main.py",
+        "--character", info["character"],
+        "--output_size", info["output_size"],
+        "--simplify", info["simplify"],
+        "--output_webcam", info["output_webcam"],
+        "--model", info["model"],
+        "--anime4k",
+        "--sleep", info["sleep"],
+        "--port", info["port"]
+    ]
+    subprocess.run(command)
+
+run_command()
