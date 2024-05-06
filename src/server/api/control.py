0a1640781a5da77bf096e45bce641673e0628ea7
fucloud-macbook <270928474@qq.com>
2024-05-06 14:35:39 +0800

    feat: get status of module

--- src/server/api/control.py
+++ src/server/api/control.py
@@ -2,6 +2,9 @@ import sys; sys.path.append("src")
 from flask import Blueprint
 
 from booter import BasicBooter
+from module.interface.manager import manager
+
+from .result import Result
 
 booter = BasicBooter()
 
@@ -10,7 +13,7 @@ control_api = Blueprint('control_api', __name__)
 def check_module_can_control(name: str) -> bool:
     # 不是一键控制，也部署 booter 的子模块
     # 目前只有 booter 的子模块可以单独的启动的暂停
-    return name == "all" or name in booter.sub_module_list
+    return name == "booter" or name in booter.sub_module_list
 
 @control_api.route("/module/<name>/start", methods=['POST'])
 def start(name: str):
@@ -18,7 +21,7 @@ def start(name: str):
     if not check_module_can_control(name):
         return "module is not found or can't be controlled", 400
         
-    if name  == "all":
+    if name  == "booter":
         booter.start()
     else:
         booter.start_sub_module(name)
@@ -31,16 +34,21 @@ def stop(name: str):
     if not check_module_can_control(name):
         return "module is not found or can't be controlled", 400
     
-    if name == "all":
+    if name == "booter":
         booter.stop()
     else:
         booter.stop_sub_module(name)
 
     return "ok", 200
 
-@control_api.route("/check/status", methods=['GET'])
-def check_status():
-    ...
+@control_api.route("/module/<name>/status", methods=['GET'])
+def status(name: str):
+    info = manager.info(name)
+
+    if info is None:
+        return Result.create(), 404
+    
+    return Result.create(info={"status": info.status}), 200
 
 @control_api.route("/send/message", methods=['POST'])
 def send_message():
