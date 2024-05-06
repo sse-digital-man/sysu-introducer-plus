d8b02c1c45f93f59c19727a2fa10120cd4715706
fucloud-macbook <270928474@qq.com>
2024-05-06 17:38:30 +0800

    feat: getControllableModuleList

--- src/server/api/control.py
+++ src/server/api/control.py
@@ -1,5 +1,5 @@
 import sys; sys.path.append("src") 
-from flask import Blueprint
+from flask import Blueprint, request
 
 from booter import BasicBooter
 from module.interface.manager import manager
@@ -15,7 +15,7 @@ def check_module_can_control(name: str) -> bool:
     # 目前只有 booter 的子模块可以单独的启动的暂停
     return name == "booter" or name in booter.sub_module_list
 
-@control_api.route("/module/<name>/start", methods=['POST'])
+@control_api.route("/module/start/<name>", methods=['POST'])
 def start(name: str):
     # 验证启动模块请求 
     if not check_module_can_control(name):
@@ -29,7 +29,7 @@ def start(name: str):
     return "ok", 200
     
 
-@control_api.route("/module/<name>/stop", methods=['POST'])
+@control_api.route("/module/stop/<name>", methods=['POST'])
 def stop(name: str):
     if not check_module_can_control(name):
         return "module is not found or can't be controlled", 400
@@ -41,7 +41,7 @@ def stop(name: str):
 
     return "ok", 200
 
-@control_api.route("/module/<name>/status", methods=['GET'])
+@control_api.route("/module/status/<name>", methods=['GET'])
 def status(name: str):
     info = manager.info(name)
 
@@ -49,6 +49,28 @@ def status(name: str):
         return Result.create(), 404
     
     return Result.create(info={"status": info.status}), 200
+
+@control_api.route("/module/list/controllable", methods=['GET'])
+def get_module_can_control():
+    with_booter = request.args.get("withBooter") not in [0, None]
+
+    infos = [manager.info(sub_module) for sub_module in booter.sub_module_list]
+    
+    if with_booter:
+        infos.append(booter.info)
+
+    result = []
+
+    for info in infos:    
+        result.append({
+            "alias": info.alias,
+            "name": info.name,
+            "kind": info.kind,
+            "status": info.status
+        })
+
+    return Result.create(info={"list": result}), 200
+    
 
 @control_api.route("/send/message", methods=['POST'])
 def send_message():
