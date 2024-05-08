04cde7e9ca06533fac550bad1cb0bd040fdcd8db
Marco <marcowky@qq.com>
2024-05-08 20:01:49 +0800

    modify 修改睡眠间隙

--- src/test/view/api.py
+++ src/test/view/api.py
@@ -2,15 +2,15 @@ import sys
 sys.path.append("./src")
 from module.view import view
 
-res = view.speack("D:/codefile/sysu-introducer-plus/src/module/view/data/speech/uri_speech_0.wav")
+# res = view.speack("D:/codefile/sysu-introducer-plus/src/module/view/data/speech/uri_speech_0.wav")
 
 # res = view.sing("D:/codefile/sysu-introducer-plus/src/module/view/data/song/不分手的恋爱-汪苏泷.MP3", "D:/codefile/sysu-introducer-plus/src/module/view/data/song/不分手的恋爱-汪苏泷_voice.MP3", mouth_offset=0.5, beat=2)
 
 # res = view.swing(
     # "D:/codefile/sysu-introducer-plus/src/module/view/data/music/Noisy Radio - Chipi Chipi Chapa Chapa.flac")
 
-# res = view.stop_move()
+res = view.stop_move()
 
-# print(res)
+print(res)
 
-view.start()
+# view.start()
