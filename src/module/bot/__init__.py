a27c413f727ff816afcb264329f0b049cb01c999
fucloud-macbook <270928474@qq.com>
2024-05-10 17:09:54 +0800

    refactor: move search logic to SimpleBot

--- src/module/bot/__init__.py
+++ src/module/bot/__init__.py
@@ -1,8 +1,4 @@
-from .kind import CallerKind
 from ..interface import BasicModule
-from .searcher.es import EsSearcher
-from .caller.gpt import GptCaller
-
 
 BOT = "bot"
 BASE_PATH = f"module.{BOT}"
@@ -12,31 +8,9 @@ class BasicBot(BasicModule):
 
     def __init__(self):
         super().__init__(BOT)
-        self._es = EsSearcher()
-        self._gpt = GptCaller()
 
     def _load_config(self):
-        self._gpt._load_config()
         pass
 
     def talk(self, query: str) -> str:
-        content= "你现在的任务是找出问题的关键词，注意:提取的关键词中需要将“中大”更名为“中山大学”，下面是一个示例:\n Q:请问一下中大在2024年发生了什么? A:中山大学,校史,2024年 \n 现在的问题是" + query
-        # 1.预处理问题,得到关键词
-        p_query = self._gpt.single_call(content , False)
-        # print("--预处理后得到的关键词为:", p_query)
-        
-        # 2.使用es查询得到相似问题
-        sim_query = self._es.search(p_query, 3)
-        # print(sim_query)
-
-        # 3.生成最后的交互结果
-        final_query = "请回答问题:" + query + "下面是参考资料,如果参考资料与问题无关,请注意介绍人的身份并礼貌回答:"
-        # 3.生成最后的交互结果
-        for _id, _query in enumerate(sim_query):
-            final_query += "\n参考资料"+ str(_id+1) +":"+ _query
-        print(final_query)
-        return self._gpt.single_call(final_query , True)
-    
-
-    def caller_kind(self) -> CallerKind:
-        return self._caller.kind
+        return self._sub_module("caller").single_call(query , True)
--- src/module/bot/simple.py
+++ src/module/bot/simple.py
@@ -1,17 +1,35 @@
-from ..interface import BasicModule
+from . import BasicBot
 
-
-BOT = "bot"
-BASE_PATH = f"module.{BOT}"
-
-class SimpleBot(BasicModule):
-    system_prompt = "你现在是一名主播，请回答观众问题，请将回答控制在10字以内。"
-
+class SimpleBot(BasicBot):
     def __init__(self):
-        super().__init__(BOT)
+        super().__init__()
 
     def _load_config(self):
         pass
 
     def talk(self, query: str) -> str:
-        return self._sub_module("caller").single_call(query)
+        caller = self._sub_module("caller")
+        searcher = self._sub_module("searcher")
+
+        # 如果Searcher为空，直接使用调用器返回
+        if searcher is None:
+            return caller.single_call(query , True)
+
+        content= "你现在的任务是找出问题的关键词，注意:提取的关键词中需要将“中大”更名为“中山大学”，下面是一个示例:\n Q:请问一下中大在2024年发生了什么? A:中山大学,校史,2024年 \n 现在的问题是" + query
+
+        # 1.预处理问题,得到关键词
+        p_query = caller.single_call(content , False)
+        # print("--预处理后得到的关键词为:", p_query)
+        
+        # 2.使用es查询得到相似问题
+        sim_query = searcher.search(p_query, 3)
+        # print(sim_query)
+
+        # 3.生成最后的交互结果
+        final_query = "请回答问题:" + query + "下面是参考资料,如果参考资料与问题无关,请注意介绍人的身份并礼貌回答:"
+        
+        for _id, _query in enumerate(sim_query):
+            final_query += "\n参考资料"+ str(_id+1) +":"+ _query
+        # print(final_query)
+
+        return caller.single_call(final_query , True)
