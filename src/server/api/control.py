f8654a53f8d973f84314d8a7f49e288c62e26155
fucloud-macbook <270928474@qq.com>
2024-05-10 21:45:49 +0800

    feat: union the ErrorCode about Response

--- src/server/api/control.py
+++ src/server/api/control.py
@@ -1,43 +1,66 @@
 from typing import Dict
 from flask import Blueprint, request
 
+from module.interface.info import moduleStatusMap
 from utils.config import config
 from server import manager
 
-from .result import Result
+from .result import Result, ErrorCode
 
-booter = manager.object("booter")
 control_api = Blueprint('control_api', __name__)
 
+booter = manager.object("booter")
+
 def check_module_can_control(name: str) -> bool:
-    # 不是一键控制，也部署 booter 的子模块
-    # 目前只有 booter 的子模块可以单独的启动的暂停
-    return name == "booter" or name in booter.sub_module_list
-
-@control_api.route("/module/start/<name>", methods=['POST'])
-def start(name: str):
-    # 验证启动模块请求 
-    if not check_module_can_control(name):
-        return "module is not found or can't be controlled", 400
-        
-    if name  == "booter":
-        booter.start()
+    if manager.object(name) == None:
+        return Result.create(code=ErrorCode.ModuleNotFound)
+    elif name == "booter" or name in booter.sub_module_list:
+        # 目前只有 booter 与其子模块才可以单独地启动的暂停
+        return None
     else:
-        booter.start_sub_module(name)
+        return Result.create(code=ErrorCode.ModuleUncontrollable)
 
-    return "ok", 200
+@control_api.route("/module/<regex('start|stop'):control>/<name>", methods=['POST'])
+def start(name: str, control: str):
+    # 验证启动模块请求 
+    result = check_module_can_control(name)
 
+    if result is not None:
+        return result
+    
+    if control == "start":
+        flag, status = booter.start_sub_module(name) \
+            if name != "booter" else booter.start()
+    elif control == "stop":
+        flag, status = booter.stop_sub_module(name) \
+            if name != "booter" else booter.stop()
+    
+    # 如果模块启动失败，则说明是当前状态不支持
+    if not flag:
+        return Result.create(
+            code=ErrorCode.ModuleStatusNotSupported,
+            data={ 
+                "status": status, 
+                "statusLabel": moduleStatusMap[status] 
+            })
+
+    return Result.create()
+
+'''
 @control_api.route("/module/stop/<name>", methods=['POST'])
 def stop(name: str):
-    if not check_module_can_control(name):
-        return "module is not found or can't be controlled", 400
+    # 验证启动模块请求 
+    result = check_module_can_control(name)
+    if result is not None:
+        return result
     
     if name == "booter":
         booter.stop()
     else:
         booter.stop_sub_module(name)
 
-    return "ok", 200
+    return Result.create()
+'''
 
 @control_api.route("/module/change/<name>", methods=['PUT'])
 def change_module_kind(name: str):
@@ -45,11 +68,27 @@ def change_module_kind(name: str):
     try:
         kind = data["kind"]
     except:
-        return Result.create(), 400
+        return Result.create(code=ErrorCode.KeyDataMissing)
 
     manager.change_module_kind(name, kind)
 
-    return "ok", 200
+    return Result.create()
+
+@control_api.route("/module/config/<name>", methods=['PUT'])
+def modify_module_config(name: str):
+    try:
+        data = request.get_json()
+
+        kind = data["kind"]
+        content: Dict = data["content"]
+
+        config.update(name, kind, content, save=True)
+    except BaseException:
+        # 出现未知的
+        return Result.create()
+    
+
+    return Result.create()
 
 '''
 @control_api.route("/module/status/<name>", methods=['GET'])
@@ -90,7 +129,7 @@ def get_controllable_module():
 
 @control_api.route("/module/list/all", methods=['GET'])
 def get_all_module():
-    return Result.create(data={"list": manager.module_info_list}), 200
+    return Result.create(data={"list": manager.module_info_list})
 
 @control_api.route("/module/config/<name>", methods=['GET'])
 def get_module_config(name: str):
@@ -101,22 +140,6 @@ def get_module_config(name: str):
         return Result.create(data={
             "config": config_list, 
             # "kinds": kind_list
-        }), 200
+        })
     except KeyError:
-        return Result.create(), 404
-
-@control_api.route("/module/config/<name>", methods=['PUT'])
-def modify_module_config(name: str):
-    try:
-        data = request.get_json()
-
-        kind = data["kind"]
-        content: Dict = data["content"]
-
-        config.update(name, kind, content, save=True)
-    except BaseException:
-        # 出现未知的
-        return Result.create(), 400
-    
-
-    return Result.create(), 200
+        return Result.create(code=ErrorCode.ModuleConfigEmpty)
--- src/server/api/result.py
+++ src/server/api/result.py
@@ -1,15 +1,41 @@
-from typing import Any
+from typing import Any, Tuple, Dict
+from enum import Enum
+
 from werkzeug.wrappers import Response
 from flask import jsonify
 
+class ErrorCode(Enum):
+    # Error = (0, "错误", 500)
+    Ok = (1, "成功", 200)
+
+    KeyDataMissing = (101, "缺少关键数据", 400)
+
+    # 模块相关错误
+    ModuleNotFound =( 201, "模块不存在", 404)
+    ModuleUncontrollable = (202, "模块不可控", 400)
+    ModuleStatusNotSupported = (203, "模块状态不支持", 200)
+    ModuleConfigEmpty = (204, "模块无配置信息", 200)
+
 class Result:
     @staticmethod
-    def create(data: dict=None, code: int=0):
+    def create(data: dict=None, code: ErrorCode=ErrorCode.Ok) ->Tuple[Dict, int]:
+        """返回相应数据和状态码，默认返回成功
+
+        Args:
+            data (dict, optional): 相应数据 Defaults to None.
+            code (ErrorCode, optional): 状态码 Defaults to ErrorCode.Ok.
+
+        Returns:
+            Tuple[Dict, int]:  Flask返回的格式
+        """
+
+        item = code.value
+
         return {
             "data": data,
-            "code": code
-        }
-
+            "code": item[0],
+            "message": item[1]
+        }, item[2]
 
 class ResultResponse(Response):
     default_mimetype = "text/html; charset=utf-8"
