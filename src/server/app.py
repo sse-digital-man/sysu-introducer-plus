b453a069eeaec593c4f48626feb59c88e23b2ee1
fucloud-macbook <270928474@qq.com>
2024-05-10 14:32:52 +0800

    feat: route about webui controller

--- src/server/app.py
+++ src/server/app.py
@@ -1,21 +1,5 @@
 import sys; sys.path.append("src") 
-
-from flask import Flask
-
-from server.api import API_LIST
-from server.api.result import ResultResponse
-
-from server import ws_server
-
-app = Flask(__name__)
-
-# 注册 API
-for api in API_LIST:
-    app.register_blueprint(api)
-
-# 自定义返回类型
-app.response_class = ResultResponse
-
+from server import ws_server, app
 
 def run():
     ws_server.start()
