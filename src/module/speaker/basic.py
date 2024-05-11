1fd46011d8279f0551af23e8983ee81bafc1ff4f
kingki663 <1581690775@qq.com>
2024-05-11 17:10:28 +0800

    feat:使用ssml对 basicspeaker进行风格化

--- src/module/speaker/basic.py
+++ src/module/speaker/basic.py
@@ -3,6 +3,7 @@ import os
 import azure.cognitiveservices.speech as speechsdk
 from azure.cognitiveservices.speech import AudioDataStream
 from datetime import datetime
+from lxml import etree
 
 from module.interface import BasicModule
 
@@ -21,6 +22,58 @@ class BasicSpeaker(BasicModule):
 
         # TODO 找到合适的声音，考虑使用SSML来风格化
         self.speech_config.speech_synthesis_voice_name = "zh-CN-XiaohanNeural"
+        # TODO 筛选后合适的说话人和声音风格（后续可供控制器进行选择？）
+        self.speech_synthesis_voice_name_list = [
+            "zh-CN-XiaoyiNeural",
+            "zh-CN-XiaomengNeural",
+        ]
+        self.speech_synthesis_voice_style_list = [
+            "cheerful",
+            "hopeful",
+            "excited",
+            "friendly",
+            "gentle",
+            "whispering",
+        ]
+
+    # TODO 添加选择说话人、声音风格以及风格强度的逻辑（目前仅支持默认）
+    def _prepare_ssml(self, text: str) -> str:
+        """准备用于风格化的SSML
+
+        Args:
+            text (str): 待转化文本
+
+        Returns:
+            str: ssml文件对应的字符串
+        """
+        namespace = {
+            "xmlns": "http://www.w3.org/2001/10/synthesis",
+            "mstts": "https://www.w3.org/2001/mstts",
+        }
+        # 解析XML文件
+        tree = etree.parse(
+            "data/ssml/ssml.xml", parser=etree.XMLParser(encoding="utf-8")
+        )
+        root = tree.getroot()
+
+        # 修改name属性
+        voice_element = root.find(".//xmlns:voice", namespaces=namespace)
+        voice_element.attrib["name"] = "zh-CN-XiaoyiNeural"
+
+        # 修改style和styledegree属性
+        express_as_element = root.find(".//mstts:express-as", namespaces=namespace)
+        express_as_element.attrib["style"] = "hopeful"
+        express_as_element.attrib["styledegree"] = "2"
+
+        # 修改mstts标签内的内容
+        express_as_element.text = text
+
+        # 保存修改后的XML文件
+        tree.write("data/ssml/ssml.xml", encoding="utf-8", xml_declaration=False)
+
+        # 获取修改后的XML内容字符串
+        modified_ssml = etree.tostring(root, encoding="utf-8").decode("utf-8")
+        return modified_ssml
 
     def speak(self, text) -> str:
         if not os.path.exists(self.__output_dir):
@@ -39,7 +92,10 @@ class BasicSpeaker(BasicModule):
         )
 
         # 调用语音合成器进行语音合成，并获取合成结果
-        speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+        # speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+        # 使用ssml方式调用语音合成器进行语音合成，并获取合成结果
+        ssml_str = self._prepare_ssml(text)
+        speech_synthesis_result = speech_synthesizer.speak_ssml_async(ssml_str).get()
 
         # 根据合成结果的原因进行相应的处理
         reason = speech_synthesis_result.reason
--- src/test/speaker/basicspeaker/basicspeaker.py
+++ src/test/speaker/basicspeaker/basicspeaker.py
@@ -7,7 +7,7 @@ manager.load_modules()
 speaker = manager.object("speaker")
 
 speaker.start()
-speaker.speak("你好世界")
+speaker.speak("你好，我是中大校史介绍官中小大，很高兴能为您介绍中大校史。")
 
-module_dict = manager.info("speaker")
-print(module_dict.to_dict())
+# module_dict = manager.info("speaker")
+# print(module_dict.to_dict())
