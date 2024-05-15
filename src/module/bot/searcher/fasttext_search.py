3858c813b04e9e9f3814e15fa9fedc79958fd6b8
SmileExpression <15018135093@163.com>
2024-05-15 11:45:13 +0800

    修改代码结构

--- src/module/bot/searcher/fasttext_search.py
+++ src/module/bot/searcher/fasttext_search.py
@@ -1,4 +1,6 @@
+import json
 import chromadb
+from chromadb.api.models.Collection import Collection
 from gensim.models.fasttext import load_facebook_vectors
 from typing import List
 import time
@@ -9,7 +11,7 @@ class FTSearcher(SearcherInterface):
     使用FastText嵌入在ChromaDB数据库中搜索文本的类。
     """
 
-    def __init__(self, database_path: str, model_path: str, local_file_path: str):
+    def __init__(self):
         """
         初始化FTSearcher。
 
@@ -18,15 +20,22 @@ class FTSearcher(SearcherInterface):
             model_path: FastText模型的路径。
             local_file_path: 从本地文件加载数据的路径。
         """
+        super().__init__()
+        self.database_path = "data/chromadb"
+        self.model_path = "cc.zh.300.bin"
+        self.local_file_path = "data/database.json"
+        self.collection_id = "fasttext"
 
-        self.client = chromadb.PersistentClient(database_path)
-        collection_id = "fasttext"
-        self.collection = self.client.get_collection(name=collection_id)
-
+        self.client = chromadb.PersistentClient(self.database_path)
         # 加载FastText模型
         print("加载FastText模型...")
-        self.model = load_facebook_vectors(model_path)
+        self.model = load_facebook_vectors(self.model_path)
         print("FastText模型加载完成。")
+
+        self.collection = self.build_database()
+
+    def handle_starting(self):
+        self.build_database()
 
     def search(self, query: str, size: int) -> List[str]:
         """
@@ -48,7 +57,7 @@ class FTSearcher(SearcherInterface):
         # 返回最相似的文本
         return results["documents"][0]
 
-    def _load_config(self):
+    def load_config(self):
         pass
 
     def check(self):
@@ -56,3 +65,50 @@ class FTSearcher(SearcherInterface):
 
     def search_with_label(self):
         pass
+
+    def build_database(self) -> Collection:
+        """
+        基于数据库建立fasttext向量索引。
+        """
+
+        # 如果集合已经存在，则直接返回collection
+        #
+        # Raises:
+        #     ValueError: If the collection does not exist
+        try:
+            collection = self.client.get_collection(name=self.collection_id)
+            return collection
+        except ValueError:
+            pass
+
+        # 创建集合
+        # self.collection = self.client.get_or_create_collection(name=self.collection_id)
+
+        collection = self.create_collection(name=self.collection_id)
+
+        # 从本地文件加载数据
+        print("从本地文件加载数据...")
+        with open(self.local_file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        documents = []
+        metadatas = []
+        queries = []
+
+        for _, value in data.items():
+            documents.append(value["document"])
+            queries.append(value["query"])
+            metadata_str = value["metadata"]
+            metadata_dict = {key: True for key in metadata_str.split(",")}
+            metadatas.append(metadata_dict)
+
+        embeddings = [self.model.get_vector(text).tolist() for text in queries]
+
+        collection.add(
+            embeddings=embeddings,
+            documents=documents,
+            metadatas=metadatas,
+            ids=[str(i) for i in range(len(documents))],
+        )
+
+        print("数据收集完成。")
--- src/test/embedding/fasttext_test.py
+++ src/test/embedding/fasttext_test.py
@@ -6,11 +6,8 @@ class TestFTSearcher(unittest.TestCase):
 
     def setUp(self):
         # 请用你的实际文件路径替换这些占位符
-        database_path = "data/chromadb"
-        model_path = "cc.zh.300.bin"
-        local_file_path = "data/database.json"
 
-        self.searcher = FTSearcher(database_path, model_path, local_file_path)
+        self.searcher = FTSearcher()
 
     def test_search(self):
         # query = "中山大学校史."
