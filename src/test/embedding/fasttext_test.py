a78f389a1a82aa1f61ce37a216496650c7f027db
SmileExpression <15018135093@163.com>
2024-05-25 10:48:01 +0800

    查看内存占用

--- src/test/embedding/fasttext_test.py
+++ src/test/embedding/fasttext_test.py
@@ -1,6 +1,6 @@
 import unittest
 from module.bot.searcher.fasttext_search import FTSearcher
-
+import psutil
 
 class TestFTSearcher(unittest.TestCase):
 
@@ -8,6 +8,7 @@ class TestFTSearcher(unittest.TestCase):
         # 请用你的实际文件路径替换这些占位符
 
         self.searcher = FTSearcher()
+        self.searcher.handle_starting()
 
     def test_search(self):
         # query = "中山大学校史."
@@ -17,12 +18,18 @@ class TestFTSearcher(unittest.TestCase):
         # self.assertEqual(len(results), size)  # 检查结果的数量是否正确
         # self.assertIsInstance(results[0], str)  # 检查结果是否是字符串
         queries = [
-            "中山大学简介",
-            "中山大学成立的校史",
-            "中山大学的办学情况,校区分布",
-            "中山大学创办时间",
-            "中山大学的主管部门",
+            # "中山大学简介",
+            # "中山大学成立的校史",
+            # "中山大学的办学情况,校区分布",
+            # "中山大学创办时间",
+            # "中山大学的主管部门",
+            "中大的校长是谁？",
+            "中山大学在1944年发生了什么大事？",
         ]
+
+        # 获取当前进程的内存占用情况
+        process = psutil.Process()
+        memory_usage = process.memory_info().rss
 
         for query in queries:
             results = self.searcher.search(query, size)
@@ -31,6 +38,10 @@ class TestFTSearcher(unittest.TestCase):
             self.assertEqual(len(results), size)  # 检查结果的数量是否正确
             self.assertIsInstance(results[0], str)  # 检查结果是否是字符串
             print()
+
+        # 将字节转换为更易读的单位（如MB）
+        memory_usage_mb = memory_usage / 1024 / 1024  # 转换为MB
+        print("Memory usage:", memory_usage_mb, "MB")
 
 
 if __name__ == "__main__":
