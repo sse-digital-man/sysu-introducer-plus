2208f3fef5bed6d2393aec9ff22d10978971dea4
Marco <marcowky@qq.com>
2024-06-05 17:55:55 +0800

    modify: 修改vector的启动逻辑

--- src/module/bot/searcher/vector.py
+++ src/module/bot/searcher/vector.py
@@ -15,16 +15,26 @@ from .interface import SearcherInterface
 class VectorSearcher(SearcherInterface):
     def __init__(self):
         super().__init__()
-        
+        # 定义llm_chain和vector_store
         self.__llm_chain = None
         self.__vector_store = None
-        prompt_template = """请回答用户关于中山大学信息的查询，并且把查询连接到回答中\n查询: {query}\n回答: """
-        self.__prompt = PromptTemplate(input_variables=["query"], template=prompt_template)
 
     def handle_starting(self):
         # Notice: 不能够在 __init__() 中编写除了定义之外的操作
+        
+        # 定义prompt
+        prompt_template = """请回答用户关于中山大学信息的查询，并且把查询连接到回答中\n查询: {query}\n回答: """
+        prompt = PromptTemplate(input_variables=["query"], template=prompt_template)
+        # 获取llm实例
+        llm = OpenAI(temperature=0, openai_api_key=self.__openai_api_key, openai_api_base=self.__openai_api_base)
+        # 获取openai的embedding实例
+        base_embeddings = OpenAIEmbeddings(openai_api_key = self.__openai_api_key, openai_api_base = self.__openai_api_base)
+        
+        # llm_chain实例化
+        self.__llm_chain = LLMChain(llm=llm, prompt=prompt)
+        # vectorstore实例化
+        self.__vector_store = Chroma(persist_directory = 'data/vectorstores', embedding_function = base_embeddings)
         # self.build_index()
-        ...
 
     def search(self, query: str, size: int) -> List[str]:
         """使用elasticsearch搜索返回与 query 相似的文本列表
@@ -54,15 +64,13 @@ class VectorSearcher(SearcherInterface):
         ...
 
     def build_index(self) -> bool:
-        """基于数据库建立es索引
+        """基于数据库建立Chroma索引
         Returns:
-            bool: 是否之前就存在es索引,没有索引就建立
+            bool: 是否之前就存在Chroma索引,没有索引就建立
         """
         return False
 
     def load_config(self):
         info = self._read_config()
-        llm = OpenAI(temperature=0, openai_api_key=info["apiKey"], openai_api_base=info["url"])
-        self.__llm_chain = LLMChain(llm=llm, prompt=self.__prompt)
-        base_embeddings = OpenAIEmbeddings(openai_api_key = info["apiKey"], openai_api_base = info["url"])
-        self.__vector_store = Chroma(persist_directory = 'data/vectorstores', embedding_function = base_embeddings)
+        self.__openai_api_key = info["apiKey"]
+        self.__openai_api_base = info["url"]
