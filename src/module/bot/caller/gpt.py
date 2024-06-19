a12ef53d2d79e2834061a9ab9580d8b55131f100
Marco <marcowky@qq.com>
2024-06-19 22:39:34 +0800

    delete: 移除gpt的多余打印与注释

--- src/module/bot/caller/gpt.py
+++ src/module/bot/caller/gpt.py
@@ -33,8 +33,7 @@ class GptCaller(CallerInterface):
             messages.append({"role": "system", "content": self._system_prompt})
 
         messages.append({"role": "user", "content": query})
-        # print(self._system_prompt)
-        # print(query)
+
         for _ in range(self.__retry_n):
             try:
                 response = self.__client.chat.completions.create(
@@ -43,7 +42,6 @@ class GptCaller(CallerInterface):
                     stream=False,
                     messages=messages,
                 )
-                print(response.choices[0].message.content)
                 return response.choices[0].message.content
             except openai.APITimeoutError:
                 pass
