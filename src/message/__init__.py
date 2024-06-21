83cdeb461c9541bebf0f0a15c5b95ddde12cb6ef
yxb <yuexb@mail2.sysu.edu.cn>
2024-06-21 10:20:00 +0800

    实现了TAMQueue（基于语义和时间感知的动态消息队列）和MSQueue（基于语义分级的先入先出动态消息队列） TODO:不知道如何在queue中引入vectorsearch

--- src/message/__init__.py
+++ src/message/__init__.py
@@ -1,5 +1,5 @@
 from enum import IntEnum
-
+from datetime import datetime
 
 class MessageKind(IntEnum):
     Watcher = 0
@@ -14,9 +14,10 @@ message_kind_map = {0: "观众", 1: "管理员", 2: "指令", 3: "数字人"}
 class Message:
     def __init__(self, kind: MessageKind, content: str):
         self.kind = kind
-
         # 这里的内容既可以用户/管理员的消息，也可以存储信号类型
         self.content = content
+        self.sim = 0
+        self.timestamp = datetime.now().timestamp()
 
     def __repr__(self) -> str:
         label = message_kind_map[self.kind]
---
+++ src/module/core/msg_queue/TAM_queue.py
@@ -0,0 +1,59 @@
+from message import Message, MessageKind
+import threading
+import math
+from .dynamic_queue import DynamicMessageQueue
+from datetime import datetime
+
+
+class TAMQueue(DynamicMessageQueue):
+    def __init__(self) -> None:
+        self.queue1 = []
+        self.queue2 = []
+        self.last_time = datetime.now().timestamp()
+        self.lock = threading.Lock()
+
+    def calculate_total_priority(self, current_time, last_time, sim):
+        # 相似度计算公式为 sim*(1/(1+e^{(t-t0)/30}))
+        time_factor = 1 / (1 + math.exp((current_time - last_time)/30))
+        return sim* time_factor
+    
+    # push的时候会引起重排序
+    def push(self, message: Message):
+        with self.lock:
+            # 介绍到管理员消息时，将其放在第一队的队首
+            if message.kind == MessageKind.Admin:
+                self.queue1.insert(0, message)
+            else:
+                message.timestamp = datetime.now().timestamp()
+                # TODO:不清楚怎么这个引入searcher
+                message.sim = self._searcher.similarity(message.content)
+                self.queue2.append(message)
+                # 60s重排序一次
+                current_time = datetime.now().timestamp()
+                if (current_time - self.last_time > 60):
+                    self.queue2.sort(key=lambda x: self.calculate_total_priority(x.timestamp, current_time, x.sim))
+                    self.last_time = current_time
+
+    
+    # pop的时候不会影响
+    def pop(self) -> Message:
+        with self.lock:
+            if len(self.queue1) != 0 :
+                return self.queue1.pop(0)
+            if len(self.queue2) != 0 :
+                return self.queue2.pop(0)
+            raise ValueError("the queue is empty")
+
+
+    def clear(self):
+        with self.lock:
+            self.queue1.clear()
+            self.queue2.clear()
+
+    def empty(self) -> bool:
+        with self.lock:
+            return len(self.queue1) == 0 and len(self.queue2) == 0
+
+    def __len__(self) -> int:
+        with self.lock:
+            return len(self.queue1) + len(self.queue2)
